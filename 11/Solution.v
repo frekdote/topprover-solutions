f4f00b2afb4efa051f71be29a7d2cfb0703e1508
frekdote <frekdote@gmail.com>
2020-10-03 23:51:58 +0900

    Refactor

--- 11/Solution.v
+++ 11/Solution.v
@@ -6,78 +6,66 @@ Unset Printing Implicit Defensive.
 
 Require Import Problem.
 
-Fixpoint f (l : seq nat) :=
-  match l with
+Lemma memP (T : eqType) (x : T) (s : seq T) :
+  reflect (List.In x s) (x \in s).
+Proof.
+apply: (iffP idP).
+  by elim: s => //= y s IH; rewrite in_cons; case/orP => [/eqP | /IH]; auto.
+by elim: s => //= y s IH; rewrite in_cons; case => [-> | /IH ->]; rewrite ?eq_refl ?orbT.
+Qed.
+
+Lemma uniqP' (T : eqType) (s : seq T) :
+  reflect (List.NoDup s) (uniq s).
+Proof.
+apply: (iffP idP).
+  by elim: s => /= [_ | x s IH /andP [/memP ? /IH ?]]; constructor.
+by elim: s / => //= x s /memP -> _ ->.
+Qed.
+
+Lemma perm_count (T : eqType) (s s' : seq T) (a : pred T) :
+  perm_eq s s' -> count a s = count a s'.
+Proof.
+by move=> ?; rewrite -!size_filter; apply/perm_size/perm_filter.
+Qed.
+
+Lemma swap_once_perm s s' :
+  swap_once s s' -> perm_eq s s'.
+Proof.
+elim: s s' / => [x y s | x s s' _ IH]; last by rewrite perm_cons.
+by apply/permP => ? /=; ring.
+Qed.
+
+Fixpoint f (s : seq nat) :=
+  match s with
     [::] => 0
-  | x :: l' => count (fun y => x > y) l' + f l'
+  | x :: s' => count (fun y => x > y) s' + f s'
   end.
 
-Lemma swap_once_count l l' p :
-  swap_once l l' ->
-  count p l = count p l'.
+Lemma swap_once_odd_f s s' :
+  swap_once s s' -> uniq s -> odd (f s') = ~~ odd (f s).
 Proof.
-  elim.
-  - by move=> h1 h2 t /=; ring.
-  - by move=> h t1 t2 _ /= ->.
+elim: s s' / => /= [x y s | x s s' so IH /andP [_ /IH E]].
+  rewrite in_cons; case/and3P => /norP [+ _] _ _.
+  case: ltngtP => //= _ _.
+    by congr (~~ (odd _)); rewrite add0n addnCA.
+  by rewrite negbK add0n addnCA.
+have /perm_count -> := swap_once_perm so.
+by rewrite !odd_add E addbN.
 Qed.
 
-Lemma NoDup_swap_once_odd l l' :
-  List.NoDup l -> swap_once l l' -> odd (f l') = odd ((f l).+1).
+Lemma is_odd_permutation_odd_f s s' :
+  is_odd_permutation s s' -> uniq s -> odd (f s') = ~~ odd (f s).
 Proof.
-  move=> H H'.
-  elim: H' H.
-  - move=> h1 h2 t H.
-    inversion H; subst.
-    move: (Decidable.not_or _ _ H2) => [/eqP H22 _] /=.
-    case: ltngtP H22 => //= _ _;
-      rewrite -addnE ?negbK;
-      [ congr (~~ odd _) | congr (odd _) ];
-      ring.
-  - move=> h t1 t2 H IH H' /=.
-    inversion H'; subst.
-    rewrite !odd_add (swap_once_count _ H) IH ?addbN //=.
-Qed.
-
-Lemma swap_once_In l l' :
-  swap_once l l' -> forall x, List.In x l <-> List.In x l'.
-Proof.
-  elim.
-  - move=> h1 h2 t x /=; intuition.
-  - move=> h t1 t2 _ IH x /=; rewrite IH //.
-Qed.
-
-Lemma NoDup_swap_once_NoDup l l' :
-  List.NoDup l -> swap_once l l' -> List.NoDup l'.
-Proof.
-  move=> H H'.
-  elim: H' H.
-  - move=> h1 h2 t H.
-    inversion H; subst.
-    move: (Decidable.not_or _ _ H2) => [H21 H22].
-    apply not_eq_sym in H21.
-    inversion H3; subst.
-    constructor.
-    + by move=> [].
-    + by constructor.
-  - move=> h t1 t2 H IH H'.
-    inversion H'; subst.
-    constructor.
-    + move=> cont.
-      apply (swap_once_In H) in cont.
-      intuition.
-    + intuition.
+elim: s s' / => [| s1 s2 s3 s4 so12 so23 _ IH uniq_s1]; first by exact: swap_once_odd_f.
+have /perm_uniq/esym := swap_once_perm so12; rewrite uniq_s1 => uniq_s2.
+have /perm_uniq/esym := swap_once_perm so23; rewrite uniq_s2 => uniq_s3.
+by rewrite (IH uniq_s3) (swap_once_odd_f so23 uniq_s2) (swap_once_odd_f so12 uniq_s1) negbK.
 Qed.
 
 Theorem solution: task.
 Proof.
-  unfold task.
-  move=> l1 l2 H H' /(f_equal (fun l => odd (f l)))cont.
-  induction H'.
-  - move: cont (NoDup_swap_once_odd H H0) => /= ->.
-      by case: (odd (f l2)).
-  - move: (NoDup_swap_once_NoDup H H0) => H''.
-    move: (NoDup_swap_once_NoDup H'' H1) => H'''.
-    move: (NoDup_swap_once_odd H'' H1) (NoDup_swap_once_odd H H0) IHH' => -> /= ->.
-    rewrite negbK.
-    intuition.
+unfold task.
+move=> s s' /uniqP' un op /(f_equal (fun s => odd (f s))).
+rewrite (is_odd_permutation_odd_f op un).
+by case: (odd _).
 Qed.
