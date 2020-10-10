affc038076d73d17502453f79e2dd1db532bd899
frekdote <frekdote@gmail.com>
2020-10-10 14:43:40 +0900

    Revise #20

--- 20/Solution.v
+++ 20/Solution.v
@@ -4,97 +4,100 @@ Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
-Require Import PeanoNat.
+(******************************************************************************)
+(* Auxiliary lemmas to convert functions in the Coq standard library into the *)
+(* ones in MathComp                                                           *)
+(******************************************************************************)
+
+Require Import Arith.
+
+Lemma lebE m n : (m <=? n) = (m <= n).
+Proof. by apply/Nat.leb_spec0/leP. Qed.
+
+Lemma minE m n : Nat.min m n = minn m n.
+rewrite /minn; case: leqP => [| /ltnW] /leP.
+  exact: Nat.min_r.
+exact: Nat.min_l.
+Qed.
+
+(******************************************************************************)
+(* Instances for enabling lia to handle MathComp functions                    *)
+(******************************************************************************)
+
+Require Import ZArith ZifyClasses ZifyInst ZifyBool Lia.
+
+Instance Op_addn : BinOp addn := Op_plus.
+Add BinOp Op_addn.
+
+Instance Op_subn : BinOp subn := Op_sub.
+Add BinOp Op_subn.
+
+Instance Op_minn : BinOp minn :=
+  { TBOp := Z.min;
+    TBOpInj := ltac:(by move=> *; rewrite /= -minE; lia) }.
+Add BinOp Op_minn.
+
+Instance Op_leq : BinOp leq :=
+  { TBOp := fun x y => isLeZero (x - y);
+    TBOpInj := ltac:(by move=> *; rewrite -lebE Z_of_nat_leb_iff Z_leb_sub) }.
+Add BinOp Op_leq.
+
+Ltac lia' := unfold is_true in *; lia.
+
+(******************************************************************************)
+
 Require Import Problem.
 
-Lemma complete_induction (P : nat -> Prop) :
-  (forall n, ((forall m, m < n -> P m) -> P n)) ->
-  forall n, P n.
+Variant seq_spec : seq nat -> Type :=
+| SeqNil : seq_spec [::]
+| SeqApp s s' : seq_spec (s ++ [:: 0] ++ s')
+| SeqSucc x s : seq_spec (S x :: map S s).
+
+Lemma seqP s : seq_spec s.
 Proof.
-  move=> H.
-  have H': forall n m, m < n -> P m.
-  { elim => // n IHn m.
-    rewrite ltnS leq_eqVlt.
-    by move=> /orP[/eqP->|]; auto.
-  }
-  move=> n.
-  apply: H' (ltnSn _).
+elim: s => [| x s IH]; first by apply: SeqNil.
+case: x => [| x]; first by apply: (SeqApp [::]).
+case: s / IH => [| s s' | y s].
+- by apply: (SeqSucc x [::]).
+- by apply: (SeqApp (S x :: s) s').
+- by apply: (SeqSucc x (y :: s)).
 Qed.
 
-Lemma compute2E h1 h2 l :
-  compute2 (h1 :: h2 :: l) = h1 - Nat.min h1 h2 + compute2 (h2 :: l).
-Proof. by []. Qed.
+Lemma compute2_cons_cons h1 h2 t :
+  compute2 (h1 :: h2 :: t) = h1 - h2 + compute2 (h2 :: t).
+Proof. rewrite /=; lia'. Qed.
 
-Lemma compute2_zero l1 l2 :
-  compute2 (l1 ++ [:: 0] ++ l2) =
-  compute2 l1 + compute2 l2.
+Lemma compute2_app s s' :
+  compute2 (s ++ [:: 0] ++ s') = compute2 s + compute2 s'.
 Proof.
-  elim: l1 => [| h t]; [| case: t => [| h' t']].
-  - by case: l2.
-  - by rewrite /= Nat.min_0_r minusE subn0; case: l2.
-  - move=> IH. rewrite !compute2E IH. ring.
+elim: s => [| h1 t].
+  by case: s'.
+case: t => [_ | h2 t IH].
+  by rewrite [_ ++ _ ++ _]/= compute2_cons_cons /=; congr (_ + _); [lia' | case: s'].
+by rewrite [_ ++ _ ++ _]/= !compute2_cons_cons IH; lia'.
 Qed.
 
-Lemma compute2_S l :
-  ~~ nilp l ->
-  compute2 [ seq x.+1 | x <- l ] =
-  (compute2 l).+1.
+Lemma compute2_succ h t :
+  compute2 (map S (h :: t)) = S (compute2 (h :: t)).
 Proof.
-  elim: l => // h t; case t => // h' t' IH _.
-  rewrite [map _ _]/= 2!compute2E.
-  rewrite -Nat.succ_min_distr subSS.
-  rewrite IH //. ring.
+elim: t h => // h' t IH h.
+by rewrite [map _ _]/= !compute2_cons_cons IH; lia'.
 Qed.
 
-Lemma lem l :
-  l = [::] \/
-  (exists l1 l2, l = l1 ++ [:: 0] ++ l2) \/
-  (exists h t, l = [ seq x.+1 | x <- h :: t ]).
-Proof.
-  elim: l => [| h t H]; auto.
-  right.
-  case: (h =P 0) => [->|].
-  - by left; exists [::], t.
-  - move/Nat.neq_0_r => [h' Hh'].
-    case: H => [-> | [[l1 [l2 H12]] | [h'' [t' Ht']]]].
-    + by right; exists h', [::]; rewrite Hh'.
-    + by left; exists (h :: l1), l2; rewrite H12.
-    + by right; exists h', (h'' :: t'); rewrite Hh' Ht'.
-Qed.
+Lemma sumn_succ s :
+  sumn (map S s) = sumn s + size s.
+Proof. by elim: s => //= x s ->; lia'. Qed.
 
 Theorem solution: task.
 Proof.
-  unfold task.
-  move=> s.
-  remember (size s + sumn s) as si.
-  generalize dependent s.
-  elim/complete_induction: si.
-  move=> n IH s Heqsi.
-  case: (lem s); [| case].
-  - by move->; constructor.
-  - move=> [l1 [l2 H12]].
-    rewrite H12 compute2_zero.
-    constructor.
-    + apply (IH (size l1 + sumn l1)); auto.
-      rewrite Heqsi H12.
-      rewrite -addSn leq_add //.
-      * rewrite size_cat /= addnS ltnS leq_addr //.
-      * rewrite !sumn_cat leq_addr //.
-    + apply (IH (size l2 + sumn l2)); auto.
-      rewrite Heqsi H12.
-      rewrite -addSn leq_add //.
-      * rewrite size_cat /= addnS ltnS leq_addl //.
-      * rewrite !sumn_cat leq_addl //.
-  - move=> [h [t Hht]].
-    rewrite Hht compute2_S; auto.
-    constructor.
-    apply (IH (size (h :: t) + sumn (h :: t))); auto.
-    rewrite Heqsi Hht.
-    rewrite -addnS leq_add //.
-    * by rewrite size_map.
-    * rewrite /= addSn ltnS leq_add2l.
-      elim t => // h' t' IHt'.
-      rewrite /= addSn.
-      apply leqW.
-      rewrite leq_add2l //.
+unfold task.
+move=> s.
+have [x] := ubnP (size s + sumn s).
+elim: x => // x IH in s *.
+case: s / (seqP s) => [_ | s s' ine | y s ine].
+- exact: c1_nil.
+- rewrite !size_cat !sumn_cat /= in ine.
+  by rewrite compute2_app; apply: c1_app; apply: IH; lia'.
+- rewrite /= size_map sumn_succ in ine.
+  by rewrite compute2_succ; apply: c1_succ; apply: IH; rewrite /=; lia'.
 Qed.
