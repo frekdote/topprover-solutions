204d1741a88b260f8b4bd5f21c648ef46064bba1
frekdote <frekdote@gmail.com>
2020-10-10 16:51:11 +0900

    Refactor #23

--- 23/Solution.v
+++ 23/Solution.v
@@ -1,26 +1,30 @@
+Set Warnings "-notation-overridden".
+From mathcomp Require Import ssreflect.
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
 Require Import Problem.
-Require Import Omega.
+Require Import Arith.
 
 Lemma lem1 m :
   bin (2 * m + 1) -> bin (S (2 * m)).
-Proof. rewrite Nat.add_1_r; auto. Qed.
+Proof. by rewrite Nat.add_1_r. Qed.
 
 Lemma lem2 m :
   bin (2 * S m) -> bin (S (2 * m + 1)).
-Proof. replace (2 * S m) with (S (2 * m + 1)) by omega; auto. Qed.
+Proof. by rewrite Nat.mul_succ_r Nat.add_succ_r. Qed.
 
 Fixpoint bin_succ n (H : bin n) : bin (S n) :=
   match H with
     bin_epsilon => bin_1 0 bin_epsilon
-  | bin_0 m H' => lem1 _ (bin_1 m H')
-  | bin_1 m H' => lem2 _ (bin_0 (S m) (bin_succ m H'))
+  | bin_0 m H' => lem1 (bin_1 m H')
+  | bin_1 m H' => lem2 (bin_0 (S m) (bin_succ H'))
   end.
 
 Theorem solution: task.
 Proof.
-  unfold task.
-  intros n.
-  induction n as [| n' IH].
-  - constructor.
-  - exact (bin_succ _ IH).
+unfold task.
+elim => [| n /bin_succ //].
+exact: bin_epsilon.
 Qed.
