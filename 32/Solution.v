f0c65b4c8a8d40ae6c65e85090978a93c585076d
frekdote <frekdote@gmail.com>
2020-10-16 13:31:32 +0900

    Refactor #32

--- 32/Solution.v
+++ 32/Solution.v
@@ -4,7 +4,8 @@ Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
-Require Import Problem Omega.
+Require Import Problem.
+Require Import Lia.
 
 (* HINT *)
 Scheme le_max_ind := Induction for le Sort Prop.
@@ -15,20 +16,20 @@ Scheme le_max_ind := Induction for le Sort Prop.
 
 Theorem solution: task.
 Proof.
-  unfold task.
-  induction p using le_max_ind.
-  - move=> q.
-    rewrite (_ : le_n m = @eq_rect _ _ (fun x => m <= x) (le_n m) _ (eq_refl m)) //.
-    move: {2 4 6 10}m q (eq_refl m).
-    destruct q.
-    + move=> e. rewrite (Eqdep_dec.UIP_refl_nat _ e) //.
-    + move=> ?. subst. omega.
-  - move=> q.
-    rewrite (_ : le_S m m0 p = @eq_rect _ _ (fun x => m <= x) (le_S m m0 p) _ (eq_refl (S m0))) //.
-    move: {1 3 4 6}(S m0) q (eq_refl _).
-    destruct q.
-    + move=> ?. subst. omega.
-    + move=> e. inversion e. subst.
-      rewrite (Eqdep_dec.UIP_refl_nat _ e).
-      congr le_S; auto.
+unfold task.
+move=> m n.
+elim/le_max_ind: n / => [q | n' p' IH q].
+  rewrite -[le_n m]/(eq_rect m (fun x => m <= x) (le_n m) m (eq_refl m)).
+  (* Set Printing All. *)
+  (* move: q eq_refl. *)
+  move: {2 4 6 10}m q eq_refl => l q.
+  case: l / q => [e |]; last by lia.
+  by rewrite (Eqdep_dec.UIP_refl_nat _ e).
+rewrite -[le_S m n' p']/(eq_rect (S n') (fun x => m <= x) (le_S m n' p') (S n') eq_refl).
+(* Set Printing All. *)
+(* move: q eq_refl. *)
+move: {1 3 4 6}(S n') q eq_refl => l q.
+case: l / q => [| l' q' e]; first by lia.
+have [En'l'] := e; rewrite En'l' in p' IH q' e *.
+by rewrite (Eqdep_dec.UIP_refl_nat _ e); congr le_S.
 Qed.
